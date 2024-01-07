36bd53fcf2daa07138508368f6ef8b88dde86790
idreesalmasri <idrees.massri95@gmail.com>
2024-01-07 09:34:09 +0000

    add delete project route

--- src/models/index.sequelize.js
+++ src/models/index.sequelize.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-const {Sequelize,DataTypes}=require('sequelize');
-const dbString=process.env.DB_URL;
+const { Sequelize, DataTypes } = require("sequelize");
+const dbString = process.env.DB_URL;
 const { usersModelCreator } = require("../models/user.model");
 const { projectModelCreator } = require("./projects.model");
 const { taskModelCreator } = require("./tasks.model");
-const {teamModelCreator}=require('./teams.model')
-const {organizationModelCreator}=require('./organization.model')
+const { teamModelCreator } = require("./teams.model");
+const { organizationModelCreator } = require("./organization.model");
 let sequelizeOptions =
     process.env.NODE_ENV === "production"
         ? {
@@ -20,11 +20,11 @@ let sequelizeOptions =
         : {};
 
 const sequelize = new Sequelize(dbString, sequelizeOptions);
-const userModel=usersModelCreator(sequelize,DataTypes);
-const projectModel=projectModelCreator(sequelize,DataTypes)
-const taskModel=taskModelCreator(sequelize,DataTypes);
-const teamModel = teamModelCreator(sequelize,DataTypes);
-const organizationModel=organizationModelCreator(sequelize,DataTypes);
+const userModel = usersModelCreator(sequelize, DataTypes);
+const projectModel = projectModelCreator(sequelize, DataTypes);
+const taskModel = taskModelCreator(sequelize, DataTypes);
+const teamModel = teamModelCreator(sequelize, DataTypes);
+const organizationModel = organizationModelCreator(sequelize, DataTypes);
 
 // organizationModel.hasMany(userModel,{foreignKey:'organizationId', as:'organizationMembers'});
 // userModel.belongsTo(organizationModel, {foreignKey: "organizationId",as: "relatedOrganization"});
@@ -32,35 +32,57 @@ const organizationModel=organizationModelCreator(sequelize,DataTypes);
 // organizationModel.hasMany(projectModel, { foreignKey: "organizationId" , as:'organizationProjects'});
 // projectModel.belongsTo(organizationModel, { foreignKey: "organizationId" ,as:"relatedOrganization"});
 
-userModel.hasMany(projectModel, {foreignKey: "projectManagerId",as: "managedProjects"});
-projectModel.belongsTo(userModel, {foreignKey: "projectManagerId",as: "projectManager"});
+userModel.hasMany(projectModel, {
+    foreignKey: "projectManagerId",
+    as: "managedProjects",
+    onDelete: "CASCADE",
+});
+projectModel.belongsTo(userModel, {
+    foreignKey: "projectManagerId",
+    as: "projectManager",
+    onDelete: "CASCADE",
+});
 
-projectModel.hasMany(teamModel, { foreignKey: "projectId",as:"teams" });
-teamModel.belongsTo(projectModel, {foreignKey: "projectId",as:"relatedProject"});
+projectModel.hasMany(teamModel, {
+    foreignKey: "projectId",
+    as: "teams",
+    onDelete: "CASCADE",
+});
+teamModel.belongsTo(projectModel, {
+    foreignKey: "projectId",
+    as: "relatedProject",
+    onDelete: "CASCADE",
+});
 
 userModel.hasMany(teamModel, { foreignKey: "teamLeaderId", as: "leadedTeam" });
-teamModel.belongsTo(userModel, {foreignKey: "teamLeaderId",as: "teamLeader"});
+teamModel.belongsTo(userModel, {
+    foreignKey: "teamLeaderId",
+    as: "teamLeader",
+});
 
-userModel.belongsToMany(teamModel,{through:"user_team",as:"memberOf"});
-teamModel.belongsToMany(userModel,{through:"user_team",as:"teamMembers"});
+userModel.belongsToMany(teamModel, { through: "user_team", as: "memberOf" });
+teamModel.belongsToMany(userModel, { through: "user_team", as: "teamMembers" });
 
 // teamModel.hasMany(taskModel, { foreignKey: "teamId", as:"tasks" });
 // taskModel.belongsTo(teamModel, { foreignKey: "teamId",as:"assigneeTeam" });
 
-projectModel.hasMany(taskModel, { foreignKey: "projectId", as: "tasks" });
+projectModel.hasMany(taskModel, {
+    foreignKey: "projectId",
+    as: "tasks",
+    onDelete: "CASCADE",
+});
 taskModel.belongsTo(projectModel, {
     foreignKey: "projectId",
     as: "project",
     onDelete: "CASCADE",
 });
 
-userModel.hasMany(taskModel,{foreignKey:"userId",as:"assignedTasks"});
+userModel.hasMany(taskModel, { foreignKey: "userId", as: "assignedTasks" });
 taskModel.belongsTo(userModel, {
     foreignKey: "userId",
     as: "assigneeUser",
     onDelete: "CASCADE",
 });
-
 
 module.exports = {
     sequelize,
--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -5,10 +5,12 @@ const {
     createProject,
     getProject,
     getManagerProjects,
+    deleteProject,
 } = require("../controllers/project.controllers");
 
 
 Router.route('/').get(getProject).post(createProject);
 Router.get("/manager/:id", getManagerProjects);
+Router.delete("/:projectId", deleteProject);
 
 module.exports=Router;
